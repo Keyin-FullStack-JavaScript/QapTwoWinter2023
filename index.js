650ffb100d16da3edf43995e245721df5f4d15d9
Peter <113168584+prawstho@users.noreply.github.com>
2023-02-06 08:26:08 -0330

    Completed routes, html, and menu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 // Put the routes in their own file
 const routes = require('./routes.js');
 // add a global for debugging
-global.DEBUG = true;
+global.DEBUG = false;
 
 const server = http.createServer((request, response) => {
     let path = "./views/";
@@ -17,22 +17,26 @@ const server = http.createServer((request, response) => {
         case '/about':
             path += "about.html";
             if(DEBUG) console.info(path);
-            response.end('multi-route-server-2');
+            response.statusCode = 200;
+            routes.aboutPage(path, response);
             break;
         case '/contact':
             path += "contact.html";
             if(DEBUG) console.info(path);
-            response.end('multi-route-server-2');
+            response.statusCode = 200;
+            routes.contactPage(path, response);
             break;
         case '/depart':
             path += "depart.html";
             if(DEBUG) console.info(path);
-            response.end('multi-route-server-2');
+            response.statusCode = 200;
+            routes.departPage(path, response);
             break;
         case '/subscribe':
             path += "subscribe.html"
             if(DEBUG) console.info(path);
-            response.end('multi-route-server-2');
+            response.setHeader('Set-cookie', 'subscription=New');
+            routes.subscribePage(path, response);
             break;
         case '/about-me':
             // this is a redirect for a deprecated route
@@ -44,7 +48,8 @@ const server = http.createServer((request, response) => {
         default:
             path += "404.html";
             if(DEBUG) console.info(path);
-            response.end('multi-route-server-2');
+            response.statusCode = 404;
+            routes.fourOfourPage(path, response);
             break;
     }
 });
